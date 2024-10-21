f9a21ec71fc4c6b15768703575ae849f3750993f
0x3b <h4x0r@localhost.localdomain>
2024-10-22 07:30:35 +0800

    Python Backdoor 101

--- svr_cmd_exe.py
+++ svr_cmd_exe.py
@@ -1,10 +1,6 @@
 import subprocess
 
-# call method returns nothing like void
-# use check_output method to store the returned value in output variable
-
 output = subprocess.check_output("pwd", shell = True)
 print(output, "<-- directory")
 
-# convert output bytes to strings and remove newline
 print(output.decode("utf-8").replace("\n",""), "<-- directory")
